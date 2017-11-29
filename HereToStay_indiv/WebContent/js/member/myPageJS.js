c02bf870dbc3b5faba11333a1d8138a46b06c0b9
양은지 <cyzy4862@naver.com>
2017-11-29 18:45:39 +0900

    은지

--- HereToStay_indiv/WebContent/js/member/myPageJS.js
+++ HereToStay_indiv/WebContent/js/member/myPageJS.js
@@ -28,11 +28,11 @@ $(document).ready(function(){
 		// 비밀번호 공백 or 입력
 		if($('#pwdInput').val() != "") {
 			// 입력한 비밀번호가 맞는지 틀렸는지
-			if($('#memPwd').val() == $('#pwdInput').val()){
+			if($('#memberPwd').val() == $('#pwdInput').val()){
 				$('#memberInfo').attr('action',"/member/infoUpdate.do");
 				$('#memberInfo').submit();
 			} else {
-				alert('비밀번호를 다시 확인해주세요.');
+				alert('비밀번호를 다시 확인해주세요. pwdInput : ' + $('#pwdInput').val() + "> " + $('#memberPwd').val());
 			}
 			
 		} else {
